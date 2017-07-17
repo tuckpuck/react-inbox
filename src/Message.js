d45cfbfe8e4d0748663b7ffb4ac555102465ade7
Tucker Triggs <tucker.triggs@yahoo.com>
2017-07-17 11:04:38 -0600

    file tree updated, readyto begin

--- src/Message.js
+++ src/Message.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './Message.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
